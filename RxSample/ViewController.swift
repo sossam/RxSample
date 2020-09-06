964dd5c345cbb52eb43ecf8a02f85769b161a00f
sossam <heart_b_e@naver.com>
2020-09-06 17:59:48 +0900

    RxSwift 첫 도전

--- RxSample/ViewController.swift
+++ RxSample/ViewController.swift
@@ -7,14 +7,34 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
 
 class ViewController: UIViewController {
+    @IBOutlet private weak var searchBar: UISearchBar!
+    @IBOutlet private weak var tableView: UITableView!
+    
+    let allCities: [String] = ["New York", "London", "Oslo", "Warsaw", "berlin", "Praga"]
+    lazy var shownCities: BehaviorSubject<[String]> = .init(value: allCities)
+    var disposeBag: DisposeBag = .init()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        bindUI()
     }
-
-
+    
+    func bindUI() {
+        shownCities.bind(to: tableView.rx.items(cellIdentifier: "cityPrototypeCell")) { (row, city, cell) in
+            cell.textLabel?.text = city
+        }.disposed(by: disposeBag)
+        
+        searchBar.rx.text
+            .orEmpty
+            .debounce(.milliseconds(500), scheduler: MainScheduler.instance)
+            .distinctUntilChanged()
+            .filter{ !$0.isEmpty }
+            .compactMap { [weak self] query in self?.allCities.filter{ $0.hasPrefix(query) } }
+            .bind(to: shownCities)
+            .disposed(by: disposeBag)
+    }
 }
-
